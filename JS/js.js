dce6f0218e2a678240cb6bb00ecc0c99d952a7b2
Alartsp <alartsp@ukr.net>
2018-07-27 00:23:55 +0300

    add

--- JS/js.js
+++ JS/js.js
@@ -1,15 +1,24 @@
 $(".light_on").click(function() {
-    $("header").css("background-image", "url(\"images/bg_all.jpg\")");
+    $("header").css("background-image", "url(\"images/light_on_wo_box.jpg\")");
 });
 
 $(".light_off").click(function() {
     $("header").css("background-image", "none");
+    $(".boxes").css("display", "none");
 });
 
 $(".box_on").click(function() {
-    $("header").css("background-image", "url(\"images/bg_all.jpg\")");
+    $(".boxes").css("display", "block");
 });
 
 $(".box_off").click(function() {
-    $("header").css("background-image", "none");
+    $(".boxes").css("display", "none");
+});
+
+$(".light_on_under").click(function() {
+    $("main").css("background-image", "url(\"images/underground2.jpg\")");
+});
+
+$(".light_offon_under").click(function() {
+    $("main").css("background-image", "none");
 });
