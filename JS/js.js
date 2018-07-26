6bcaa3504a6108a0e06519aaec7532e6d8b630c3
Alexander Kozelskiy <AKozelskiy@starcom.kiev.ua>
2018-07-26 19:03:30 +0300

    add

---
+++ JS/js.js
@@ -0,0 +1,15 @@
+$(".light_on").click(function() {
+    $("header").css("background-image", "url(\"images/bg_all.jpg\")");
+});
+
+$(".light_off").click(function() {
+    $("header").css("background-image", "none");
+});
+
+$(".box_on").click(function() {
+    $("header").css("background-image", "url(\"images/bg_all.jpg\")");
+});
+
+$(".box_off").click(function() {
+    $("header").css("background-image", "none");
+});
