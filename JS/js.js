239363f58cbb514764f7b75781901298c5d27050
Alexander Kozelskiy <AKozelskiy@starcom.kiev.ua>
2018-07-30 17:20:14 +0300

    add

--- JS/js.js
+++ JS/js.js
@@ -24,38 +24,41 @@ $(".light_off_under").click(function() {
 });
 
 
-var arr = [{id: "1"}, {id: "2"}, {id: "3"}];
-var ul = document.querySelector(ul);
-for (var i=0; i < arr.length; i++) {
-    var li = document.createElement("li");
-    li.innerHTML = arr[i].id;
-    ul.appendChild(li);
-}
-
-
-var arr = [
+var arr =  [
     {name: 'div1', width: '50px', height:'50px', backgroundColor: 'red'},
     {name: 'div2', width: '50px', height:'50px', backgroundColor: 'blue'},
     {name: 'div3', width: '50px', height:'50px', backgroundColor: 'green'},
     {name: 'div4', width: '50px', height:'50px', backgroundColor: 'white'}
-    ]
-var ul_el = document.querySelector('.massive > ul');
-var li_el1 = document.createElement('li');
-var li_el2 = document.createElement('li');
-var li_el3 = document.createElement('li');
-var li_el4 = document.createElement('li');
-li_el1.style.cssText = 'width: 200px; height: 200px; background-color: red;';
-li_el2.style.cssText = 'width: 200px; height: 200px; background-color: blue;';
-li_el3.style.cssText = 'width: 200px; height: 200px; background-color: green;';
-li_el4.style.cssText = 'width: 200px; height: 200px; background-color: white;';
-ul_el.appendChild(li_el1);
-ul_el.appendChild(li_el2);
-ul_el.appendChild(li_el3);
-ul_el.appendChild(li_el4);
+];
+var ul = document.querySelector('.array > ul');
+for (var i=0; i < arr.length; i++) {
+    var li = document.createElement("li");
+    li.innerHTML = arr[i].name;
+    ul.appendChild(li);
+}
 
-var ul = document.querySelector('.massive > ul');
-var li = document.createElement('li');
-ul.appendChild(li);
-
+var images = [
+    'images/img1.jpg',
+    'images/img2.jpg',
+    'images/img3.jpg',
+    'images/img4.jpg'
+];
+var num = 0;
+function next() {
+    var slider = document.getElementsByClassName("slider")[0];
+    num++;
+    if(num >= images.length) {
+        num = 0;
+    }
+    slider.src = images[num];
+}
+function prev() {
+    var slider = document.getElementsByClassName("slider")[0];
+    num--;
+    if(num < 0) {
+        num = images.length-1;
+    }
+    slider.src = images[num];
+}
 
 
