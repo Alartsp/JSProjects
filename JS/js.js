8149fdbf787de0f90b1ec8cf93cd5e81e897c088
Alexander Kozelskiy <AKozelskiy@starcom.kiev.ua>
2018-07-30 15:37:42 +0300

    add

--- JS/js.js
+++ JS/js.js
@@ -24,6 +24,15 @@ $(".light_off_under").click(function() {
 });
 
 
+var arr = [{id: "1"}, {id: "2"}, {id: "3"}];
+var ul = document.querySelector(ul);
+for (var i=0; i < arr.length; i++) {
+    var li = document.createElement("li");
+    li.innerHTML = arr[i].id;
+    ul.appendChild(li);
+}
+
+
 var arr = [
     {name: 'div1', width: '50px', height:'50px', backgroundColor: 'red'},
     {name: 'div2', width: '50px', height:'50px', backgroundColor: 'blue'},
