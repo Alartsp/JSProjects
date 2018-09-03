9494cdb1f6b481a22704e66dd1e270de9fc9b4a9
Alartsp <alartsp@ukr.net>
2018-09-04 00:04:41 +0300

    asdd

--- JS/js.js
+++ JS/js.js
@@ -1,8 +1,18 @@
+UpLightSwitch();
+UnderLightSwitch();
+BoxSwitch();
+BoxMoving();
+
+
+
+
 var boxes = document.querySelector('header > div:nth-child(3)');
 var slider = document.querySelector('.slider');
-var upperLightBtn = document.querySelector('.light_upper_btn');
-upperLightBtn.addEventListener('click', ()=>{
-    var header = document.querySelector('header');
+
+function UpLightSwitch() {
+    var upperLightBtn = document.querySelector('.light_upper_btn');
+    upperLightBtn.addEventListener('click', ()=>{
+        var header = document.querySelector('header');
     var frame = document.querySelector('.frame');
     if (header.classList.contains('header_on')) {
         header.classList.remove('header_on');
@@ -21,24 +31,12 @@ upperLightBtn.addEventListener('click', ()=>{
     }
 }
 );
+};
 
-var boxBtn = document.querySelector('.box_btn');
-boxBtn.addEventListener('click', ()=>{
-    if (boxes.classList.contains('boxes')) {
-        boxes.classList.remove('boxes');
-        boxes.classList.add('boxes_off');
-        boxBtn.innerHTML = 'In';
-    } else {
-        boxes.classList.remove('boxes_off');
-        boxes.classList.add('boxes');
-        boxBtn.innerHTML = 'Out';
-    }
-}
-);
-
-var underLightBtn = document.querySelector(".light_under_btn");
-underLightBtn.addEventListener('click', ()=>{
-    var main = document.querySelector('main');
+function UnderLightSwitch() {
+    var underLightBtn = document.querySelector(".light_under_btn");
+    underLightBtn.addEventListener('click', ()=>{
+        var main = document.querySelector('main');
     if (main.classList.contains('main_on')) {
         main.classList.remove('main_on');
         main.classList.add('main_light_off');
@@ -50,65 +48,61 @@ underLightBtn.addEventListener('click', ()=>{
     }
 }
 );
+};
 
-//add div list
-var arr = [{
-    name: 'div1',
-    width: '50px',
-    height: '50px',
-    backgroundColor: 'red'
-}, {
-    name: 'div2',
-    width: '50px',
-    height: '50px',
-    backgroundColor: 'blue'
-}, {
-    name: 'div3',
-    width: '50px',
-    height: '50px',
-    backgroundColor: 'green'
-}, {
-    name: 'div4',
-    width: '50px',
-    height: '50px',
-    backgroundColor: 'white'
-}];
-
-// image slider
-var images = ['images/img1.jpg', 'images/img2.jpg', 'images/img3.jpg', 'images/img4.jpg'];
-var num = 0;
-function next() {
-    num++;
-    if (num >= images.length) {
-        num = 0;
+function BoxSwitch() {
+    var boxBtn = document.querySelector('.box_btn');
+    boxBtn.addEventListener('click', ()=>{
+        if (boxes.classList.contains('boxes')) {
+        boxes.classList.remove('boxes');
+        boxes.classList.add('boxes_off');
+        boxBtn.innerHTML = 'In';
+    } else {
+        boxes.classList.remove('boxes_off');
+        boxes.classList.add('boxes');
+        boxBtn.innerHTML = 'Out';
     }
-    slider.src = images[num];
 }
-function prev() {
-    num--;
-    if (num < 0) {
-        num = images.length - 1;
-    }
-    slider.src = images[num];
-}
-var timerId = setInterval(function() {
-    next();
-}, 5000);
+);
+};
 
-//moving boxes
-$(document).ready(function() {
-    var i = 0;
-    $('body').keydown(function(event) {
-        if (event.keyCode == 37 && i <= 6) {
-            i++;
-            boxes.style.left = 350 - i * 50 + 'px';
-        }
-        if (event.keyCode == 39 && i >= -24) {
-            i--;
-            boxes.style.left = 350 - i * 50 + 'px';
-        }
+function BoxMoving() {
+    $(document).ready(function() {
+        var i = 0;
+        $('body').keydown(function(event) {
+            if (event.keyCode == 37 && i <= 6) {
+                i++;
+                boxes.style.left = 350 - i * 50 + 'px';
+            }
+            if (event.keyCode == 39 && i >= -24) {
+                i--;
+                boxes.style.left = 350 - i * 50 + 'px';
+            }
+        });
     });
-});
+};
+
+    var images = ['images/img1.jpg', 'images/img2.jpg', 'images/img3.jpg', 'images/img4.jpg'];
+    var num = 0;
+    function next() {
+        num++;
+        if (num >= images.length) {
+            num = 0;
+        }
+        slider.src = images[num];
+    }
+    function prev() {
+        num--;
+        if (num < 0) {
+            num = images.length - 1;
+        }
+        slider.src = images[num];
+    }
+    var timerId = setInterval(function() {
+        next();
+    }, 5000);
+
+
 
 var wineStandCatalog = [{
     name: 'stand01',
@@ -156,83 +150,75 @@ var wineStandCatalog = [{
     price: '250$'
 }];
 
-function createElement(domItem) {
-    var li = document.createElement("li");
-    li.innerHTML = domItem.name;
-    li.style.width = domItem.width;
-    li.style.height = domItem.height;
-    li.style.backgroundColor = domItem.backgroundColor;
-    return li;
-}
+    function addTable(wineStands) {
+        //var table = document.createElement('table');
+        var tr = document.createElement('tr');
+        var td1 = document.createElement('td');
+        var td2 = document.createElement('td');
+        var td3 = document.createElement('td');
+        var div = document.createElement('div');
+        var img = document.createElement('img');
+        img.src = wineStands.image;
+        //table.appendChild(tr);
+        td1.innerHTML = wineStands.name;
+        td2.appendChild(img);
+        td3.innerHTML = wineStands.price;
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        return tr;
+    };
+    var mainDiv = document.querySelector('main > div');
+    var table = document.createElement('table');
+    var th1 = document.createElement('th');
+    var th2 = document.createElement('th');
+    var th3 = document.createElement('th');
+    var trm = document.createElement('tr');
+    th1.innerHTML = 'Name';
+    th2.innerHTML = 'Photo';
+    th3.innerHTML = 'Price';
+    trm.appendChild(th1);
+    trm.appendChild(th2);
+    trm.appendChild(th3);
+    table.appendChild(trm);
+    for (var i = 0; i < wineStandCatalog.length; i++) {
+        var tr = addTable(wineStandCatalog[i]);
+        mainDiv.appendChild(table);
+        table.appendChild(tr);
+    };
 
-function addTable(wineStands) {
-    //var table = document.createElement('table');
-    var tr = document.createElement('tr');
-    var td1 = document.createElement('td');
-    var td2 = document.createElement('td');
-    var td3 = document.createElement('td');
-    var div = document.createElement('div');
-    var img = document.createElement('img');
-    img.src = wineStands.image;
-    //table.appendChild(tr);
-    td1.innerHTML = wineStands.name;
-    td2.appendChild(img);
-    td3.innerHTML = wineStands.price;
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-    tr.appendChild(td3);
-    return tr;
-}
 
-var mainDiv = document.querySelector('main > div');
-var table = document.createElement('table');
-var th1 = document.createElement('th');
-var th2 = document.createElement('th');
-var th3 = document.createElement('th');
-var trm = document.createElement('tr');
-th1.innerHTML = 'Name';
-th2.innerHTML = 'Photo';
-th3.innerHTML = 'Price';
-trm.appendChild(th1);
-trm.appendChild(th2);
-trm.appendChild(th3);
-table.appendChild(trm);
-for (var i = 0; i < wineStandCatalog.length; i++) {
-    var tr = addTable(wineStandCatalog[i]);
-    mainDiv.appendChild(table);
-    table.appendChild(tr);
-}
+    function addCatalog(wineStands) {
+        var li = document.createElement("li");
+        var div = document.createElement('div');
+        var pName = document.createElement('p');
+        var img = document.createElement('img');
+        var pPrice = document.createElement('p');
+        pName.innerHTML = wineStands.name;
+        pPrice.innerHTML = wineStands.price;
+        img.src = wineStands.image;
+        li.appendChild(div);
+        div.appendChild(pName);
+        div.appendChild(img);
+        div.appendChild(pPrice);
+        return li;
+    };
 
-function addCatalog(wineStands) {
-    var li = document.createElement("li");
-    var div = document.createElement('div');
-    var pName = document.createElement('p');
-    var img = document.createElement('img');
-    var pPrice = document.createElement('p');
-    pName.innerHTML = wineStands.name;
-    pPrice.innerHTML = wineStands.price;
-    img.src = wineStands.image;
-    li.appendChild(div);
-    div.appendChild(pName);
-    div.appendChild(img);
-    div.appendChild(pPrice);
-    return li;
-}
+    var ul = document.createElement('ul');
+    for (var i = 0; i < wineStandCatalog.length; i++) {
+        var li = addCatalog(wineStandCatalog[i]);
+        mainDiv.appendChild(ul);
+        ul.appendChild(li);
+    };
 
-var ul = document.createElement('ul');
-for (var i = 0; i < wineStandCatalog.length; i++) {
-    var li = addCatalog(wineStandCatalog[i]);
-    mainDiv.appendChild(ul);
-    ul.appendChild(li);
-}
 
-var changeStyleBtn = document.querySelector('.view_btn');
-var mainTable = document.querySelector('.array > table');
-var mainUl = document.querySelector('.array > ul');
-mainTable.style.display = 'none';
-mainUl.style.display = 'block';
-changeStyleBtn.addEventListener('click', ()=>{
-    var arrayDiv = document.querySelector('.array');
+    var changeStyleBtn = document.querySelector('.view_btn');
+    var mainTable = document.querySelector('.array > table');
+    var mainUl = document.querySelector('.array > ul');
+    mainTable.style.display = 'none';
+    mainUl.style.display = 'block';
+    changeStyleBtn.addEventListener('click', ()=>{
+        var arrayDiv = document.querySelector('.array');
     if ((mainTable.style.display == 'none') && (mainUl.style.display == 'block')) {
         mainTable.style.display = 'table';
         mainUl.style.display = 'none';
@@ -242,6 +228,8 @@ changeStyleBtn.addEventListener('click', ()=>{
     }
 }
 );
+
+
 
 var upperMenu = [{
     name: 'About Us',
